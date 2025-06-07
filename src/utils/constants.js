13a0a0402bed89f0e1123b60e9ec8c903d7aa5e5
Yijun <me@yijun.hu>
2025-06-07 21:34:35 +0100

    Parallax header complete

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const BgImage = {
+    DRAGON: "/images/oyz.webp"
+};
+
+export const Link = {
+    HOME: {name: "Home", href: "/"},
+    STORIES: {name: "Stories", href: "/stories"},
+    BACK: {name: "Back", href: "../"},
+};
+
+export const Links = {
+    HSLINKS: [Link.HOME, Link.STORIES],
+    ALLLINKS: [Link.HOME, Link.STORIES, Link.BACK]
+};
